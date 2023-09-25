5e94268ee92859fbaf7ea558940ead7932964310
KATHERINE YASBETH TENECELA MEJIA <katherine.tenecela@ucuenca.edu.ec>
2023-09-25 14:22:46 -0500

    se cambio color

--- sketch.js
+++ sketch.js
@@ -3,5 +3,12 @@ function setup() {
 }
 
 function draw() {
-  circle(mouseX, mouseY, 20);
+ background(255);
+ fill(255,200,200);
+ ellipse(60,90,16,16);
+ strokeWeight(mouseY);
+ circle(windowWidth / 2, windowHeight / 2, 300);
+ textSize(20);
+ text(mouseX, 50, 50);
+ text(int(mouseY), 50, 100);
 }
